5142116149ab5c25f80783a04ce97d2b6d2c8a95
ellakrechmer <ekrechmer20@stuy.edu>
2021-01-03 16:22:42 -0500

    wrote and tested pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -7,7 +7,7 @@ public class PigLatin{
       while (l.hasNext()) {
         Scanner w=new Scanner(l.next());
         while (w.hasNext()) {
-          System.out.print(pigLatinSimple(w.next()));
+          System.out.print(pigLatin(w.next()));
         }
         if (l.hasNext()) System.out.print(" ");
       }
@@ -16,13 +16,27 @@ public class PigLatin{
     System.out.println();
   }
   public static String pigLatinSimple(String s){
+    s=s.toLowerCase();
     char first=s.charAt(0);
+    if (s.length()==1){
+      if (first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u'){
+        return s+"hay";
+      }
+      return s+"ay";
+    }
     if (first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u'){
       return s+"hay";
     }
-    else if (s.length()>1){
-      return s.substring(1)+first+"ay";
+    else return s.substring(1)+first+"ay";
+  }
+  public static String pigLatin(String s){
+    ArrayList<String> digraphs = new ArrayList<>(Arrays.asList(new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"}));
+    s=s.toLowerCase();
+    if (s.length()>1){
+      if (digraphs.contains(s.substring(0,2))){
+        return s.substring(2)+s.substring(0,2)+"ay";
+      }
     }
-    else return s+"ay";
+    return pigLatinSimple(s);
   }
 }
